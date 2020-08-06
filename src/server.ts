15327bd04bb6d732ebdf4e618b2b01a2f6c4906f
Rai Medeiros <raismedeiros@gmail.com>
2020-08-06 14:08:58 -0300

    Configuração do heroku

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,14 @@ import express from 'express'
 import routes from './routes'
 import cors from 'cors'
 
+const PORT : string|number = process.env.PORT || 3333;
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(routes)
+app.use("*",(req, res) =>{
+  res.send("<h1>Welcome to your simple server! Awesome right</h1>");
+});
 
-app.listen(process.env.PORT || 3333)
+app.listen(PORT,()=>console.log(`🚀 Server started on port ${PORT}`)) 
