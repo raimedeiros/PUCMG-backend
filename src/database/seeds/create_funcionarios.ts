496708ca57a55bbdada8faea1b87f87005b60b64
Rai Medeiros <raismedeiros@gmail.com>
2020-08-14 23:35:32 -0300

    corre ção de banco

--- src/database/seeds/create_funcionarios.ts
+++ src/database/seeds/create_funcionarios.ts
@@ -2,10 +2,10 @@ import Knex from 'knex'
 import {hash} from 'bcryptjs'
 
 export async function seed(knex: Knex) {
-    const password = await hash('PUCMG', 8);
+    const password = await hash('TCCPUCMG', 8);
     await knex('funcionarios').insert([
         { name: 'Gilberto Braga',email:"gilberto@despensa.com",password},
-        { name: 'Felícia Braga',email:"felicia@espensa.com",password},
-        { name: 'Maria Pacheco',email:"maria@espensa.com",password},
+        { name: 'Felícia Braga',email:"felicia@despensa.com",password},
+        { name: 'Maria Pacheco',email:"maria@despensa.com",password},
     ])
 }
